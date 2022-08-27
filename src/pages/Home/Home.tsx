1b17be10fcbbc7e60a0ea23ebc90544330d2f41b
Gary Kumar <gary.kumar@akqa.com>
2022-08-27 15:35:39 +1000

    Add error boundary to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,11 @@
+import ErrorBoundary from "../../components/ErrorBoundary";
+
 export const Home = () => {
-  return <div>Check 2</div>;
+  return (
+    <ErrorBoundary>
+      <div>Check</div>
+    </ErrorBoundary>
+  );
 };
 
 export default Home;
