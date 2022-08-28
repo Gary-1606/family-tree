e4da23c3687b51dbee265c6ff2f4cfdf2dc8caaf
Gary Kumar <gary.kumar@akqa.com>
2022-08-28 13:56:01 +1000

    Config changed for image import

--- jest.config.ts
+++ jest.config.ts
@@ -7,17 +7,8 @@ const config: Config.InitialOptions = {
     "!**/node_modules/**",
   ],
   moduleNameMapper: {
-    "^@$": "<rootDir>/src",
-    "^@/(.*)\\.(?!jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/src/$1",
-    // // "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-    // //   "<rootDir>/src/mocks/fileMock.js",
-    // "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-    //   "<rootDir>/src/__mocks__/fileMock.js",
-    // "\\.(css|less)$": "<rootDir>/src/mocks/fileMock.js",
-
-    // // The regex above, doesn't work properly
-    // "^.+\\.(svg|png)": `<rootDir>/src/mocks/fileMock.js`,
+    "^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
 
     // Handle module aliases
     "^@shared/(.*)$": "<rootDir>/src/shared/$1",
