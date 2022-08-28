05dfc5c6933ae0db4415eece9a49835dfd11fe8a
Gary Kumar <gary.kumar@akqa.com>
2022-08-28 11:57:17 +1000

    Implement code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { render } from "react-dom";
-import { FunctionComponent, StrictMode } from "react";
+import { FunctionComponent, StrictMode, lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Result from "./pages/Result";
+
+const Result = lazy(() => import("./pages/Result"));
 
 const App: FunctionComponent = () => {
   return (
     <StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/result" element={<Result />} />
-        </Routes>
-      </BrowserRouter>
+      <Suspense fallback={<h2>Loading....</h2>}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/result" element={<Result />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </StrictMode>
   );
 };
